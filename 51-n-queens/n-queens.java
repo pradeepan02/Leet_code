a56d4fb328b6e6a6a0ee4d0e26f3d22b42d6c7a4
pradeepan02 <124977260+pradeepan02@users.noreply.github.com>
2025-01-31 09:04:20 +0530

    Time: 1 ms (99.80%) | Memory: 44.7 MB (86.81%) - LeetSync

--- 51-n-queens/n-queens.java
+++ 51-n-queens/n-queens.java
@@ -1,54 +1,32 @@
 class Solution {
-    private boolean isSafePlace(int n, char[][] nQueens, int row, int col) {
-        for (int i = 0; i < n; i++) {
-            if (nQueens[i][col] == 'Q') {
-                return false;
-            }
+     List<String> perm=new ArrayList<>();
+    List<List<String>> res=new ArrayList<>();
+    static int n, comb[]=new int[9];
+    public List<List<String>> solveNQueens(int n) {
+        this.n=n;
+        char[] ch=new char[n];
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<n;j++)
+                ch[j]=i==j? 'Q': '.';
+            perm.add(new String(ch));
         }
-
-        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
-            if (nQueens[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
-            if (nQueens[i][j] == 'Q') {
-                return false;
-            }
-        }
-
-        return true;
+        f(0, 0, 0, 0);
+        return res;
     }
 
-    private void solveNQueens(int n, List<List<String>> output, char[][] nQueens, int row) {
-        if (row == n) {
-            List<String> solution = new ArrayList<>();
-            for (char[] rowArray : nQueens) {
-                solution.add(new String(rowArray));
-            }
-            output.add(solution);
+    private void f(int row, int col, int diagonal, int anti_d) {
+        if(row==n) {
+            List<String> r=new ArrayList<>();
+            for(int i=0; i<n; i++) r.add(perm.get(comb[i]));
+            res.add(r);
             return;
         }
 
-        for (int col = 0; col < n; col++) {
-            if (isSafePlace(n, nQueens, row, col)) {
-                nQueens[row][col] = 'Q';
-                solveNQueens(n, output, nQueens, row + 1);
-                nQueens[row][col] = '.';
+        for(int i=0, mask=col|diagonal|anti_d; i<n; i++) {
+            if((mask&(1<<i))==0) {
+                comb[row]=i;
+                f(row+1, col|(1<<i), (diagonal|(1<<i))<<1, (anti_d|(1<<i))>>1);
             }
         }
-    }
-
-    public List<List<String>> solveNQueens(int n) {
-        List<List<String>> output = new ArrayList<>();
-        char[][] nQueens = new char[n][n];
-        
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(nQueens[i], '.');
-        }
-        
-        solveNQueens(n, output, nQueens, 0);
-        return output;
     }
 }
