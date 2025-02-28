8c86a3923c25b910bd34535502a5ec46d07fe628
Pradeepan S <124977260+pradeepan02@users.noreply.github.com>
2025-02-28 22:22:17 +0530

    Time: 1 ms (100.00%) | Memory: 42.7 MB (40.23%) - LeetSync

--- 65-valid-number/valid-number.java
+++ 65-valid-number/valid-number.java
@@ -1,8 +1,37 @@
-import java.util.regex.*;
-
 class Solution {
     public boolean isNumber(String s) {
-        String regex = "^[+-]?(((\\d+\\.\\d*)|(\\.\\d+))|(\\d+))([e|E][+-]?\\d+)?$";
-        return Pattern.matches(regex, s);
+        boolean seenDec = false;
+        boolean seenExp = false;
+        boolean seenDigit = false;
+
+        if(s.equals("")) return false;
+
+        for(int i = 0; i < s.length();i++){
+
+            char c = s.charAt(i);
+
+            if(Character.isDigit(c)) seenDigit = true;
+
+            //encounting sign
+            else if(c == '-' || c == '+'){
+                if(i > 0 && (s.charAt(i-1) != 'e' && s.charAt(i-1) != 'E')) return false;
+            }
+            
+            //encountering exponent
+            else if(c == 'e' || c == 'E'){
+                if(seenExp || !seenDigit) return false;
+                seenExp = true;
+                seenDigit = false;
+            }
+
+            //encountering decimal
+            else if(c == '.'){
+                if(seenDec || seenExp) return false;
+                seenDec = true;
+            }
+
+            else return false;
+        }
+        return seenDigit;
     }
 }
