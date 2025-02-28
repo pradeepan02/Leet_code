6cd4f5f0768e144dd691287092c74a2097cc201e
Pradeepan S <124977260+pradeepan02@users.noreply.github.com>
2025-02-28 22:21:08 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (80.40%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,22 +1,33 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        
-        for (int i = 1; i < m; i++) {
-            grid[i][0] += grid[i-1][0];
+        int row = grid.length - 1;
+        int col = grid[0].length - 1;
+
+        while(row >= 0 || col >= 0){
+            minPathSum(grid, row, col);
+            row--;
+            col--;
         }
-        
-        for (int j = 1; j < n; j++) {
-            grid[0][j] += grid[0][j-1];
-        }
-        
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+
+        return grid[0][0];
+    }
+
+    private void minPathSum(int[][] grid, int row, int col){
+        if(row < 0 || col < 0) return;
+        int right = col + 1;
+        int down = row + 1;
+        for(int r = row; r >= 0; r--){
+            if(r + 1 < grid.length || right < grid[0].length){
+                grid[r][col] += Math.min(r + 1 < grid.length ? grid[r +  1][col] : Integer.MAX_VALUE,
+                right < grid[0].length ? grid[r][right] : Integer.MAX_VALUE);
             }
         }
-        
-        return grid[m-1][n-1];
+
+        for(int c = col - 1; c >= 0; c--){
+            if(c + 1 < grid[0].length || down < grid.length){
+                grid[row][c] += Math.min(c + 1 < grid[0].length ? grid[row][c + 1] : Integer.MAX_VALUE,
+                down < grid.length ? grid[down][c] : Integer.MAX_VALUE);
+            }
+        }
     }
 }
